470824a080b7e0e29c9bff2c1ebef1c3c8ccf1ee
o-jill <nobaokix@gmail.com>
2022-06-07 21:34:32 +0900

    added 9.txt.

--- tools/rename.sh
+++ tools/rename.sh
@@ -15,3 +15,4 @@ mv kifu000000005.txt kifu${prefix}000000005.txt
 mv kifu000000006.txt kifu${prefix}000000006.txt
 mv kifu000000007.txt kifu${prefix}000000007.txt
 mv kifu000000008.txt kifu${prefix}000000008.txt
+mv kifu000000009.txt kifu${prefix}000000009.txt
