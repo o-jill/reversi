15870359b85ecff35671cc7ab76e529c2ddbc01d
o-jill <nobaokix@gmail.com>
2022-06-08 22:27:58 +0900

    made more flexible.

--- tools/rename.sh
+++ tools/rename.sh
@@ -6,13 +6,11 @@ else
   prefix=$1
 fi
 
-mv kifu000000000.txt kifu${prefix}000000000.txt
-mv kifu000000001.txt kifu${prefix}000000001.txt
-mv kifu000000002.txt kifu${prefix}000000002.txt
-mv kifu000000003.txt kifu${prefix}000000003.txt
-mv kifu000000004.txt kifu${prefix}000000004.txt
-mv kifu000000005.txt kifu${prefix}000000005.txt
-mv kifu000000006.txt kifu${prefix}000000006.txt
-mv kifu000000007.txt kifu${prefix}000000007.txt
-mv kifu000000008.txt kifu${prefix}000000008.txt
-mv kifu000000009.txt kifu${prefix}000000009.txt
+for entry in ./kifu*.txt
+do
+  #~ path=${entry#./}
+  path=${entry#./kifu}
+  #~ echo cp "$entry" "$prefix$path"
+  #~ cp "$entry" "$prefix$path"
+  cp "$entry" "kifu$prefix$path"
+done
